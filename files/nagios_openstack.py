5e0ae35d3361768900fe5ae9e37343f70b46e292
Edgar Magana <emagana@gmail.com>
2012-08-26 22:59:49 -0700

    Adding python code to get the list of available servers in this openstack deployment

---
+++ files/nagios_openstack.py
@@ -0,0 +1,26 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright 2012 Cisco Systems, Inc.  All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# @author: Edgar Magana, Cisco Systems Inc.
+
+import os
+
+f=os.popen("cobbler system list")
+for i in f:
+     j = i[:-1]
+     host_file_nagios = ("%s_nagios2.cfg" % j).strip()
+     os.popen("cp localhost_nagios2.cfg %s" %host_file_nagios)
+     os.popen ("perl -p -i -e \"s/localhost/" + host_file_nagios + "/g\" %s" %host_file_nagios) 
