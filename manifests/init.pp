0af6c3afbcbb9420becc32da9a6e2c029e9ebca2
Edgar Magana <emagana@gmail.com>
2012-08-26 23:13:24 -0700

    Adding the execution of the python code

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,6 +20,24 @@ class nagios {
   package { 'nagios3':
         ensure => installed,
   }
+
+  exec { "get-all-available-hosts":
+                command => "/usr/bin/python /etc/nagios3/conf.d/nagios_openstack.py",
+                refreshonly => true,
+                require => Package[cobbler],
+                require => File['/etc/nagios3/conf.d/nagios_openstack.py']
+        }
+
+  file { "/etc/nagios3/conf.d/nagios_openstack.py":
+        notify  => Service["nagios3"],
+        ensure  => present,
+        owner   => "root",
+        group   => "root",
+        mode    => 0755,
+        source  => "puppet:///modules/nagios/nagios_openstack.py",
+        require => Package["nagios3"],
+    }
+
  
   file { "/etc/nagios3/conf.d/services_nagios2.cfg":
         notify  => Service["nagios3"],
